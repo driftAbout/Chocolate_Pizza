a2a5d501539fee51c3f5deb1be06093b24c8449b
Kevin MIller <kevin@driftabout.com>
2017-10-28 21:42:33 -0700

    tweaking

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 var recipe = document.getElementsByClassName('recipe')[0];
 
+//add event listener to each li in the recipe list
 var recipe_li_arr = recipe.getElementsByTagName('li');
 for (var li = 0; li < recipe_li_arr.length; li++){
   console.log('recipe', recipe_li_arr[li]);
@@ -11,7 +12,17 @@ for (var li = 0; li < recipe_li_arr.length; li++){
 function checkList(e) {
   console.log('click', e.target);
   e.preventDefault();
-  e.target.classList.toggle('strike');
-  var check = e.target.getElementsByClassName('check');
+  var el_target = e.target;
+  var el_tag = e.target.tagName;
+  console.log('el_tag: ', el_tag);
+  //if the item clicked was the span in the li
+  //redirect the target tot eh li
+  if (el_tag === 'SPAN'){
+    el_target = el_target.closest('li');
+  }
+  console.log('el_target: ', el_target);
+  var check = el_target.getElementsByClassName('check');
   check[0].classList.toggle('show_check');
+  el_target.classList.toggle('strike');
+
 }
